0ef1c0aa2cadd00dddfc68e98642ac8ac6cad4d2
Michael Bidnyk <mbidnyk1@gmail.com>
2020-07-10 12:49:44 -0500

    update to sprint problems

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -3,12 +3,28 @@ class Ticket:
     def __init__(self, source, destination):
         self.source = source
         self.destination = destination
-
+        
+flight_table = {}
 
 def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
     # Your code here
+    for ticket in tickets:
+        flight_table[ticket.source] = ticket.destination
+    return print(flight_table)
 
-    return route
+ticket_1 = Ticket("PIT", "ORD")
+ticket_2 = Ticket("XNA", "SAP")
+ticket_3 = Ticket("SFO", "BHM")
+ticket_4 = Ticket("FLG", "XNA")
+ticket_5 = Ticket("NONE", "LAX")
+ticket_6 = Ticket("LAX", "SFO")
+ticket_7 = Ticket("SAP", "SLC")
+ticket_8 = Ticket("ORD", "NONE")
+ticket_9 = Ticket("SLC", "PIT")
+ticket_10 = Ticket("BHM", "FLG")
+tickets = [ticket_1, ticket_2, ticket_3, ticket_4, ticket_5,
+            ticket_6, ticket_7, ticket_8, ticket_9, ticket_10]
+reconstruct_trip(tickets,10)
